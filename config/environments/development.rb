76ae886e67f3a86ec018e55f5c35a39053ad5d6a
Ruperto <ru.bugueno@gmail.com>
2023-08-22 15:21:52 -0400

    ojala ultimo commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,23 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # ActionMailer config
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => “utf-8”
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_options = { from: 'ru.bugueno@gmail.com' }
+  config.action_mailer.smtp_settings = {
+  address: “smtp.gmail.com”,
+  port: 587,
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: Rails.application.credentials.db[:username]  ,
+  password: Rails.application.credentials.db[:password],
+  openssl_verify_mode: 'none'
+  }
+  config.action_mailer.perform_caching = false
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
